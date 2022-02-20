302c3e6d00da664ddfa604b16777ead434604f18
tdninja1 <jmack23@uncc.edu>
2022-02-19 19:57:21 -0500

    Player can fling themselves over leaves to climb as well as grapple.

--- Assets/BNG Framework/Scripts/Helpers/HandPhysics.cs
+++ Assets/BNG Framework/Scripts/Helpers/HandPhysics.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 namespace BNG {
